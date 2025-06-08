44db80c66aa078ce3d9604aa8390faa59924d11c
Kamil Gębala <kamilgebala323@gmail.com>
2025-06-08 18:10:08 +0200

    poprawka

--- src/main/java/org/example/CityBuilder.java
+++ src/main/java/org/example/CityBuilder.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 public class CityBuilder {
     public static void main(String[] args) {
-        System.out.println();
+        System.out.println();1
         System.out.println("Witaj w Fantasy City Builder - grze budowania fantastycznej osady w terminalu!");
         new Gra().menuGlowne();
     }
